0b48c3517d55d6cb62e8a427be58944e25ec22b5
ggalli <guilhermegalli5@hotmail.com>
2018-11-03 14:41:44 -0300

    generate routes

--- app/router.js
+++ app/router.js
@@ -7,6 +7,11 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
+  this.route('about');
+  this.route('timeline');
+  this.route('qualifications');
+  this.route('portfolio');
+  this.route('contact');
 });
 
 export default Router;
